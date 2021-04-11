b554b361f06ff00b28bf92ab8880082cbb389a57
Vitaly Gins <vitaly.gins@gmail.com>
2021-04-11 09:04:18 -0600

    Added Raise Check Turns and multiplayer

---
+++ public/js/client.mjs
@@ -0,0 +1,177 @@
+//HTML elements
+let clientId = null;
+let gameId = null;
+
+let gamePot = 0;
+let roundBet = 0;
+
+let playerRoundBet = 0;
+let playerChips = null;
+let playerSeat = null;
+
+let ws = new WebSocket("ws://localhost:9090");
+
+const divPlayers = document.getElementById("divPlayers");
+
+//wiring events
+const btnJoin = document.getElementById("btnJoin");
+const txtGameId = document.getElementById("txtGameId");
+
+// Join game listener
+btnJoin.addEventListener("click", () => {
+  let username = document.getElementById("username").value;
+  if (gameId === null) gameId = txtGameId.value;
+
+  const payLoad = {
+    method: "join",
+    clientId,
+    gameId,
+    username,
+  };
+
+  ws.send(JSON.stringify(payLoad));
+});
+
+// Create a new game listener
+const btnCreate = document.getElementById("btnCreate");
+btnCreate.addEventListener("click", () => {
+  let username = document.getElementById("username").value;
+  const payLoad = {
+    method: "create",
+    clientId,
+    username,
+  };
+
+  ws.send(JSON.stringify(payLoad));
+});
+
+/***************Check****************/
+const btnCheck = document.getElementById("check");
+btnCreate.addEventListener("click", () => {
+  if (roundBet > playerRoundBet) return;
+  const payLoad = {
+    method: "check",
+    clientId,
+    gameId,
+  };
+});
+
+/***************Raise****************/
+const btnRaise = document.getElementById("raise");
+btnRaise.addEventListener("click", () => {
+  const raise = document.getElementById("raiseAmount");
+  //const chips = document.querySelector(".player>.chip-count");
+
+  let raiseAmount = parseInt(raise.value);
+  //let chipsCount = parseInt(chips.innerText);
+
+  //checks for fake bets
+  if (raiseAmount <= 0) return;
+
+  // You can't bet more chips than you have
+  raiseAmount = raiseAmount < playerChips ? raiseAmount : playerChips;
+
+  const payLoad = {
+    method: "raise",
+    gameId,
+    clientId,
+    raiseAmount,
+  };
+
+  ws.send(JSON.stringify(payLoad));
+});
+
+ws.onmessage = (message) => {
+  //message.data
+  const res = JSON.parse(message.data);
+  //Just loaded page received Globally Unique Identifier
+  if (res.method === "connect") {
+    clientId = res.clientId;
+    document.querySelector("#playerId").innerText = clientId;
+  }
+
+  //A game create request has been processed
+  if (res.method === "create") {
+    gameId = res.game.id;
+    document.querySelector("#gameId").innerText = gameId;
+  }
+
+  //updated game state from server
+  if (res.method === "update") {
+    const game = res.game;
+    const clientId = document.querySelector("#playerId").innerText;
+    roundBet = game.table.roundRaise;
+    while (divPlayers.firstChild) divPlayers.removeChild(divPlayers.firstChild);
+
+    game.clients.forEach((client) => {
+      const player = document.createElement("div");
+      // Distinguishes between player and other users
+      if (client.clientId === clientId) {
+        player.classList.add("player");
+        playerChips = client.chipCount;
+        playerSeat = client.seat;
+      } else {
+        player.classList.add("opponent");
+      }
+
+      player.innerText = "Name:";
+
+      const username = document.createElement("span");
+      username.classList.add("username");
+      username.innerText = client.username;
+      player.appendChild(username);
+
+      player.innerHTML += " Chips:";
+
+      const chipCount = document.createElement("span");
+      chipCount.classList.add("chip-count");
+      chipCount.innerText = client.chipCount;
+      player.appendChild(chipCount);
+
+      player.innerHTML += ` Seat: ${client.seat}`;
+
+      divPlayers.appendChild(player);
+
+      const gamePot = document.querySelector(".game-pot");
+      gamePot.innerText = game.table.pot;
+      const playerTurn = document.querySelector(".player-turn");
+      playerTurn.innerText = `It is ${
+        game.clients[game.table.turn].username
+      }'s Turn`;
+    });
+  }
+
+  //A new player joins
+  if (res.method === "join") {
+    const game = res.game;
+    const clientId = document.querySelector("#playerId").innerText;
+
+    while (divPlayers.firstChild) divPlayers.removeChild(divPlayers.firstChild);
+
+    game.clients.forEach((client) => {
+      const player = document.createElement("div");
+      // Distinguishes between player and other users
+      if (client.clientId === clientId) {
+        player.classList.add("player");
+      } else {
+        player.classList.add("opponent");
+      }
+
+      player.innerText = "Name:";
+
+      const username = document.createElement("span");
+      username.classList.add("username");
+      username.innerText = client.username;
+      player.appendChild(username);
+
+      player.innerHTML += " Chips:";
+
+      const chipCount = document.createElement("span");
+      chipCount.classList.add("chip-count");
+      chipCount.innerText = client.chipCount;
+      player.appendChild(chipCount);
+
+      divPlayers.appendChild(player);
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,37 +1,148 @@
-require('dotenv').config();
-const express = require('express');
+const http = require("http");
+const websocketServer = require("websocket").server;
 
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const getUniqueID = require("./lib/getUniqueID");
+const getSeat = require("./lib/getSeat");
+const respondAllClients = require("./lib/respondAllClients");
 
-app.use(express.static(path.join(__dirname, './public')));
+// Express
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+app.listen(9091, () => console.log("Listening on http port 9091"));
 
-app.use(express.urlencoded({ extended: true }));
+// WebSocket
+const httpServer = http.createServer();
+httpServer.listen(9090, () => console.log("Listening.. on 9090"));
+//hashmap clients
+const clients = {};
+const games = {};
 
-app.set('view engine', 'ejs');
+const wsServer = new websocketServer({
+  httpServer: httpServer,
+});
+wsServer.on("request", (req) => {
+  //connect
+  const connection = req.accept(null, req.origin);
+  connection.on("open", () => console.log("Connection opened!"));
+  connection.on("close", () => console.log("Connection closed!"));
+  connection.on("message", (message) => {
+    const res = JSON.parse(message.utf8Data);
 
-app.use('/', (req, res, next) => {
-  res.locals.siteName = 'Cocktail Drink Generator';
-  next();
+    //a user want to create a new game
+    if (res.method === "create") {
+      const clientId = res.clientId;
+      const gameId = getUniqueID();
+      games[gameId] = {
+        id: gameId,
+        clients: [],
+        table: {
+          pot: 0,
+          round: 0, // preflop, flop, turn, river
+          turn: 0,
+          dealer: 0,
+          roundRaise: 0,
+          seats: [],
+          clients: [], // or keep client info in seats(this array is so that other players don't know each other's cards);
+        },
+      };
+      // Set all seats to empty
+      for (let i = 0; i < 10; i++) {
+        games[gameId].table.seats.push({ empty: true });
+      }
+
+      const payLoad = {
+        method: "create",
+        game: games[gameId],
+      };
+
+      const con = clients[clientId].connection;
+      con.send(JSON.stringify(payLoad));
+    }
+
+    //a client want to join an existing game
+    if (res.method === "join") {
+      const { clientId, gameId, username } = res;
+      const chipCount = res.chipCount || 5000;
+      const game = games[gameId];
+
+      if (game.clients.length >= 10) {
+        //sorry max players reach
+        return;
+      }
+
+      const seat = getSeat(game.table.seats);
+      // TODO: switch to passing the table instead of all the clients
+      game.table.seats[seat] = {
+        empty: false,
+        clientId,
+        seat,
+        username,
+        chipCount,
+        bets: [0, 0, 0, 0],
+      };
+      game.clients.push({
+        clientId,
+        username,
+        chipCount,
+        seat,
+      });
+
+      //start the game
+      if (game.clients.length >= 3) updateGameState();
+
+      const payLoad = {
+        method: "join",
+        game: game,
+      };
+
+      respondAllClients(clients, game, payLoad);
+    }
+
+    if (res.method === "raise") {
+      const { clientId, gameId, raiseAmount } = res;
+      const game = games[gameId];
+
+      game.table.roundRaise += raiseAmount;
+      game.table.pot += raiseAmount;
+      game.clients.forEach((client) => {
+        if (client.clientId === clientId) client.chipCount -= raiseAmount;
+      });
+      game.table.turn += 1;
+      game.table.turn =
+        game.table.turn >= game.clients.length ? 0 : game.table.turn;
+      updateGameState();
+    }
+  });
+
+  //generate a new clientId
+  const clientId = getUniqueID();
+  clients[clientId] = {
+    connection: connection,
+  };
+
+  const payLoad = {
+    method: "connect",
+    clientId: clientId,
+  };
+  //send back the client connect
+  connection.send(JSON.stringify(payLoad));
 });
 
-app.use((req, res, next) => {
-  const err = new Error('Requested page could not be found');
-  err.status = 404;
-  next(err);
-});
+function updateGameState() {
+  //{"gameid", fasdfsf}
+  for (const g of Object.keys(games)) {
+    const game = games[g];
+    const payLoad = {
+      method: "update",
+      game: game,
+    };
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  console.error(err);
-  const status = err.status || 500;
-  res.locals.status = status;
-  res.status(status);
-  res.render('layout', { pageTitle: `Error ${status}`, template: 'error' });
-});
+    respondAllClients(clients, game, payLoad);
+  }
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
+  setTimeout(updateGameState, 500);
+}
