08658adc70063439bde18777e69b6571262e6d2f
Dustin Shulz <dustin.shulz@edu.sait.ca>
2021-04-19 13:18:46 -0600

    adjusting styles

--- public/js/menu.mjs
+++ public/js/menu.mjs
@@ -6,6 +6,9 @@ const toggleSideBar = () => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
   overlay.classList.toggle("active");
+  const playNow = document.querySelector(".play-now::before");
+  playNow.style.setProperty("--transform-rotate-var", "-45deg");
+  playNow.style.setProperty("--transform-bg-color", "rgba(255, 255, 255, 0)");
 };
 
 burger.addEventListener("click", toggleSideBar);
